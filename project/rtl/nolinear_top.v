e5c8a0edf4284eaad4d836af8cba8c54b7d9be6b
Geek3600 <2741057934@qq.com>
2025-10-21 03:47:10 -0400

    finish vector core integrate

--- project/rtl/nolinear_top.v
+++ project/rtl/nolinear_top.v
@@ -7,9 +7,10 @@ module nolinear_top #(
     input rst,
     input en,
     input [1:0] mode, // 00: softmax, 01: gelu, 10:silu, 11:root
-    // input [DATA_NUM * FIX_POINT_WIDTH - 1:0] in,
     input [DATA_NUM * 8 - 1:0] in,
-    output [DATA_NUM * FIX_POINT_WIDTH - 1:0] out
+    output [DATA_NUM * FIX_POINT_WIDTH - 1:0] out,
+    output reg busy,
+    output done
 );  
     wire max_en;
     wire [2:0] s_in;
@@ -20,25 +21,65 @@ module nolinear_top #(
     wire en_mult;
     wire valid;
     wire finish;
+    assign done = finish; // 完成信号
 
+    reg [DATA_NUM * 8 - 1:0] in_reg;
+    reg [1:0] mode_reg;
+    always@(posedge clk) begin
+        if (rst) begin
+            in_reg <= 0;
+            mode_reg <= 0;
+        end
+        else if (en) begin
+            in_reg <= in;
+            mode_reg <= mode;
+        end
+        else begin
+            in_reg <= in_reg;
+            mode_reg <= mode_reg;
+        end
+    end
 
     wire [DATA_NUM * FIX_POINT_WIDTH - 1:0] in_fixed_vec;
-    genvar i;
+    genvar i; 
     generate
         for (i = 0; i < DATA_NUM; i = i + 1) begin : to_fixed
-            assign in_fixed_vec[i*FIX_POINT_WIDTH +: FIX_POINT_WIDTH] = {in[i*8 +: 8], 8'b0};
+            assign in_fixed_vec[i*FIX_POINT_WIDTH +: FIX_POINT_WIDTH] = {in_reg[i*8 +: 8], 8'b0};
         end
     endgenerate
 
-    controller #(
+
+    reg [1:0] mode_d;
+    reg en_d;    
+    always@(posedge clk) begin
+        if (rst) begin
+            mode_d <= 0;
+            en_d <= 0;
+        end
+        else begin
+            mode_d <= mode;
+            en_d <= en;
+        end
+    end 
+
+    always@(posedge clk) begin
+        if (rst) busy <= 0;
+        else if (en) busy <= 1;
+        else if (busy && done) busy <= 0;
+        else busy <= busy; 
+    end
+
+
+    controller #( 
         .Bf(Bf),
         .FIX_POINT_WIDTH(FIX_POINT_WIDTH),
         .DATA_NUM(DATA_NUM)
     ) u_controller (
         .clk(clk),
         .rst(rst),
-        .en(en),
-        .mode(mode), // 00: softmax, 01: gelu, 10:silu, 11:root
+        .en(en_d),
+        .mode(mode_reg), // 00: softmax, 01: gelu, 10:silu, 11:root
+
         .valid(valid), // 用于启动反馈循环，也就是开启2轮
         .max_en(max_en),
         .s_in(s_in),
@@ -58,8 +99,9 @@ module nolinear_top #(
     )  u_nolinear (
         .clk(clk),
         .rst(rst),
+        .mode(mode_reg),  // 00: softmax, 01: gelu, 10:silu, 11:root
+
         .valid(valid), // 用于启动反馈循环，也就是开启2轮
-        .mode(mode),  // 00: softmax, 01: gelu, 10:silu, 11:root
         .max_en(max_en),
         .in(in_fixed_vec),
         .s_in(s_in),
